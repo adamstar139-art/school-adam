b99f3ba2a96255995ab677eafa8e43248f4454b0
adamstar139-art <adamstar139@gmail.com>
2026-09-18 00:03:21 +0300

    Fix indentation for else statement in app.py

--- app.py
+++ app.py
@@ -2115,7 +2115,7 @@ st.markdown(clean_html(print_header_html), unsafe_allow_html=True)
 table_html = build_html_grade_table(df_students, is_blank=show_blank)
 st.markdown(clean_html(table_html), unsafe_allow_html=True)
         
-
+else:
     # استدعاء الدالة هنا داخل الـ else
     table_html = build_html_grade_table(df_students, is_blank=show_blank)
     st.markdown(clean_html(table_html), unsafe_allow_html=True)
